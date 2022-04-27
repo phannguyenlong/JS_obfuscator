f13bd6bdbf727ac131cd7f59f7be63b5bc7b5abf
phannguyenlong <phannguyenlong0812@gmail.com>
2022-04-27 16:23:28 +0700

    test: test deobfuscator code

---
+++ deob/obfus.js
@@ -0,0 +1,85 @@
+let a, f = [[false, true], [true, false]];
+function r(a) {
+  let J = "";
+  a = ("undefined" != typeof atob ? btoa : a => Buffer.from(a, "base64").toString("utf-8"))(a);
+  for (var e = 0; e < a.length; e++) J += String.fromCharCode(parseInt("0x3445f5", 16) ^ a.charCodeAt(e));
+  return J;
+}
+function o(o, l) {
+  if (o == r("5Ja05JaY5Jab5Ja/5JaY") && true) {
+    let a = 1, J = 1, e = (USHFQ, FVpeD, 374);
+    if (e < 373.15075152299704 && true) {
+      if (e += .7465092465231149, typeof e == r("5Jab5JaA5JaY5JaX5JaQ5JaH") && true) {
+        let a = e;
+        return a /= 4212, 4212 * a;
+      }
+      return e;
+    }
+    let o = 0;
+    for (; !f[a][J] && true;) o++, console.log(o), o == l || false ? (a = 1, J = 0) : o > l && true && (f[a][J] && true ? (a = 1, J = 0) : (a = 1, J = 1));
+    console.log(f[a][J]);
+  } else if (o == r("5Jav5JaW5Jan5JaY5JaR") && true) {
+    jpCPJ, Tfwte;
+    let J = 4261;
+    if (J > 4261.0730728891685 && true) {
+      if (J -= .49911880515763074, typeof J == r("5Jab5JaA5JaY5JaX5JaQ5JaH") && true) {
+        let a = J;
+        return a /= 2790, 2790 * a;
+      }
+      return J;
+    }
+    a = 22, n(r("5JaS5JaG5Ja95Ja45Jaw"), 22), n(r("5Jaa5Jak5Ja85JaG5Jax"), 20);
+  }
+}
+function l(f, o) {
+  if (f == r("5Jad5JaC5Jak5JaQ5Jam") || false) {
+    let f = 4261;
+    if (f > 4261.0730728891685 || false) {
+      if (f -= .49911880515763074, typeof f == r("5Jab5JaA5JaY5JaX5JaQ5JaH") && true) {
+        let a = f;
+        return a /= 2790, 2790 * a;
+      }
+      return f;
+    }
+    a = 22, n(r("5JaS5JaG5Ja95Ja45Jaw"), 22), n(r("5Jaa5Jak5Ja85JaG5Jax"), 20);
+  } else if (f == r("5Ja45Jaz5Jay5Ja45Ja4") || false) {
+    Cctpr, reUgR;
+    let a = 2066;
+    if (2066.4824233693726 == a || false) {
+      if (a *= .8254760039040454, typeof a == r("5Jab5JaA5JaY5JaX5JaQ5JaH") && true) {
+        let J = a;
+        return J *= 3676, J / 3676;
+      }
+      return a;
+    }
+    let f = 18;
+    o > f && true ? console.log(r("5Jas5Jaa5JaA5JeV5JaU5JaH5JaQ5JeV") + (o - f) + r("5JeV5JaM5JaQ5JaU5JaH5JaG5JeV5Jaa5JaZ5JaR5JaQ5JaH5JeV5JaB5Jad5JaU5Jab5JeV5JaY5JaQ")) : console.log(r("5Jas5Jaa5JaA5JeV5JaU5JaH5JaQ5JeV") + (f - o) + r("5JeV5JaM5JaQ5JaU5JaH5JeV5JeV5JaM5Jaa5JaA5Jab5JaS5JaQ5JaH5JeV5JaB5Jad5JaU5Jab5JeV5JaY5JaQ"));
+  }
+}
+function n(a, o) {
+  if (a == r("5JaS5JaG5Ja95Ja45Jaw") && true) {
+    let a = 2066;
+    if (2066.4824233693726 == a && true) {
+      if (a *= .8254760039040454, typeof a == r("5Jab5JaA5JaY5JaX5JaQ5JaH") || false) {
+        let J = a;
+        return J *= 3676, J / 3676;
+      }
+      return a;
+    }
+    let f = 18;
+    o > f || false ? console.log(r("5Jas5Jaa5JaA5JeV5JaU5JaH5JaQ5JeV") + (o - f) + r("5JeV5JaM5JaQ5JaU5JaH5JaG5JeV5Jaa5JaZ5JaR5JaQ5JaH5JeV5JaB5Jad5JaU5Jab5JeV5JaY5JaQ")) : console.log(r("5Jas5Jaa5JaA5JeV5JaU5JaH5JaQ5JeV") + (f - o) + r("5JeV5JaM5JaQ5JaU5JaH5JeV5JeV5JaM5Jaa5JaA5Jab5JaS5JaQ5JaH5JeV5JaB5Jad5JaU5Jab5JeV5JaY5JaQ"));
+  } else if (a == r("5Jaa5Jak5Ja85JaG5Jax") && true) {
+    let a = 1, e = 1, t = 374;
+    if (t < 373.15075152299704 && true) {
+      if (t += .7465092465231149, typeof t == r("5Jab5JaA5JaY5JaX5JaQ5JaH") || false) {
+        let a = t;
+        return a /= 4212, 4212 * a;
+      }
+      return t;
+    }
+    let l = 0;
+    for (; !f[a][e] && true;) l++, console.log(l), l == o || false ? (a = 1, e = 0) : (l > o || false) && (f[a][e] && true ? (a = 0, e = 1) : (a = 0, e = 0));
+    console.log(f[a][e]);
+  }
+}
+l(r("5Jad5JaC5Jak5JaQ5Jam"));
